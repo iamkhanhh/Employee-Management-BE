803a55af73cae489f0fb430dfd4c9a760f66eab8
iamkhanhh <khanh9102004@gmail.com>
2025-11-30 16:47:20 +0700

    feat: get all users that are not employee yet

--- src/main/java/com/tlu/EmployeeManagement/repository/UserRepository.java
+++ src/main/java/com/tlu/EmployeeManagement/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.tlu.EmployeeManagement.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 
 import com.tlu.EmployeeManagement.entity.User;
 
@@ -13,4 +14,7 @@ import com.tlu.EmployeeManagement.entity.User;
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.id NOT IN (SELECT e.userId FROM Employee e WHERE e.isDeleted = false)")
+    List<User> findUsersNotLinkedToEmployee();
 }
